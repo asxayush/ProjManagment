c205ce799ea1a577bcf0760661b1f6630fa03cd6
Ayush Sinha <as.ayushh1@gmail.com>
2026-07-09 23:22:16 +0530

    task controllers

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -7,13 +7,30 @@ import {ApiResponse} from "../utils/api-response.js"
 import {ApiError} from "../utils/api-error.js"
 import mongoose, { Mongoose } from "mongoose"
 import { AvailableUserRole, UserRolesEnum } from "../utils/constant.js"
+import { MIMEType } from "util"
 
 
 const getTasks = asyncHandler(async(req, res) => {
-    //chai
+  
 })
 const createTasks = asyncHandler(async(req, res) => {
-    //chai
+    const {title, description, assignedTo, status} = req.body
+    const {projectId} = req.params
+    const project = await Project.findById(projectId)
+    if(!project){
+        throw new ApiError(404, "project not found")
+    }
+    const files = req.files || []
+
+    const attachments = files.map((file) => {
+        return {
+            url: `${process.env.SERVER_URL}/images/${file.originalname}`,
+            mimetype: file.mimetype,
+            size: file.size
+        }
+    })
+
+    const task = await Task.create({})
 })
 const getTaskById = asyncHandler(async(req, res) => {
     //chai
